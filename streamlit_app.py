f9b9d13e511483ed645ce17971d487a84577727e
PGZON <prathameshgadgil2005@gmail.com>
2025-02-18 20:38:53 +0530

    litle bit changed

--- streamlit_app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-# Example to ensure Streamlit displays output
-st.title('Car Price Prediction')
-st.write('This is a simple example to ensure Streamlit displays output correctly.')
